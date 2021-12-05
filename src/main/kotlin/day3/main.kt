8a4a8831213b4f9790e68114eb1cb6c4a247b399
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-12-05 19:55:41 +0000

    Day 3

---
+++ src/main/kotlin/day3/main.kt
@@ -0,0 +1,44 @@
+package day3
+
+import readInput
+
+fun main() {
+    Main().also {
+        println(it.partOne()) // 4191876
+        println(it.partTwo()) // 3414905
+    }
+}
+
+typealias Binary = List<Int>
+fun Binary.toNumber() = mapIndexed { idx, elem -> if (elem == 0) 0 else 1 shl (size - idx - 1) }.sumOf { it }
+fun Int.inverse(size: Int) = ((1 shl size) - 1) - this
+
+class Main {
+    private val input: List<Binary> = readInput(3) { str ->
+        str.trim().map { it.digitToInt() }
+    }
+
+    private val gamma = mostCommon(input)
+
+    private tailrec fun filterByPosition(arr: List<Binary>, idx: Int = 0, mostCommon: Boolean = true): Binary =
+        if (arr.size == 1) {
+            arr.first()
+        } else {
+            val common = mostCommon(arr)
+            filterByPosition(arr.filter { (it[idx] == common[idx]) xor !mostCommon }, idx + 1, mostCommon)
+        }
+
+    private fun mostCommon(arr: List<Binary>) = arr.reduce { prev, elem ->
+        prev.zip(elem).map {
+            it.first + it.second
+        }
+    }.map { if (it >= arr.size / 2.0) 1 else 0 }
+
+    fun partOne() = gamma.toNumber().let { gamma ->
+        gamma * gamma.inverse(input[0].size)
+    }
+
+    fun partTwo() =
+        filterByPosition(input).toNumber() * // oxygenGenerator
+        filterByPosition(input, mostCommon = false).toNumber() // co2Scrubber
+}
