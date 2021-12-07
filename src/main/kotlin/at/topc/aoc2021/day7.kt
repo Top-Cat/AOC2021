fd3300b06f27d3001c7f4da6f7c81d62040356b1
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-12-07 15:23:13 +0000

    Alternate day 7

--- src/main/kotlin/at/topc/aoc2021/day7.kt
+++ src/main/kotlin/at/topc/aoc2021/day7.kt
@@ -1,6 +1,7 @@
 package at.topc.aoc2021
 
 import kotlin.math.abs
+import kotlin.math.min
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
@@ -20,9 +21,26 @@ class Day7 {
 
     private fun triangle(n: Int): Int = (n * (n + 1)) / 2
 
-    fun partOne() = minFuel()
+    fun partOne() = minByDescent()
 
-    fun partTwo() = minFuel(::triangle)
+    fun partTwo() = minByDescent(::triangle)
+
+    private fun fx(guess: Int, block: (Int) -> Int = { it }) = input.sumOf { n -> block(abs(n - guess)) }
+
+    private tailrec fun minByDescent(block: (Int) -> Int = { it }, min: Int = 1, max: Int = maxInput): Int {
+        val avg = (min + max) / 2
+        // Check slope at point
+        val a = fx(avg, block)
+        val b = fx(avg - 1, block)
+
+        return if (max - min < 2) {
+            min(a, b)
+        } else if (a < b) {
+            minByDescent(block, avg, max)
+        } else {
+            minByDescent(block, min, avg)
+        }
+    }
 
     private fun minFuel(block: (Int) -> Int = { it }) = (1..maxInput).minOf {
         input.sumOf { n -> block(abs(n - it)) }
